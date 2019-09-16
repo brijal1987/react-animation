db7fa48c3fd47c9f35e31b0af0e6f3d5f7dd1613
Brijal Savaliya <brijalsavaliya@Brijal-Savaliya.local>
2019-09-16 15:58:56 +0530

    added files

---
+++ build/precache-manifest.c66d64a0d6702036ac5f54d888ce06a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe229534836eea08d128bb564768a3e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb853b25232122a394b8",
+    "url": "/static/css/2.3fad095e.chunk.css"
+  },
+  {
+    "revision": "879dd1b0ee61433005e6",
+    "url": "/static/css/main.84c71541.chunk.css"
+  },
+  {
+    "revision": "bb853b25232122a394b8",
+    "url": "/static/js/2.9a4ddc69.chunk.js"
+  },
+  {
+    "revision": "879dd1b0ee61433005e6",
+    "url": "/static/js/main.aaa174c6.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
